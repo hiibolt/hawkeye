04fcd44c4f92d6718fda07a5e67ccb7792a7b9f8
hiibolt <me@hiibolt.com>
2025-02-16 23:52:09 -0600

    partial: Boilerplated tooltips for Running page

--- src/routes/pages/running.rs
+++ src/routes/pages/running.rs
@@ -115,17 +115,17 @@ pub async fn running(
         alert: None,
         jobs,
         table_entries: vec![
-            ("Job Start", "start_time", "start_time", "", false),
-            ("Queue", "queue", "queue", "", false),
-            ("Walltime", "req_walltime", "req_walltime", "", false),
-            ("# of CPUs", "req_cpus", "req_cpus", "", false),
-            ("# of GPUs", "req_gpus", "req_gpus", "", false),
-            ("Memory", "req_mem", "req_mem", "GB", false),
-            ("CPU Usage", "cpu_efficiency", "cpu_efficiency", "", true),
-            ("Memory Usage", "mem_efficiency", "mem_efficiency", "", true),
-            ("Walltime Usage", "walltime_efficiency", "walltime_efficiency", "", true)
+            (("Start Time", "<b>Job Start Time</b><br><br>Not to be confused with submission time"), "start_time", "start_time", "", false),
+            (("Queue", "<b>Job Queue</b><br><br>The queue in which the job was designated"), "queue", "queue", "", false),
+            (("Rsvd Time", "<b>The amount of reserved walltime</b>"), "req_walltime", "req_walltime", "", false),
+            (("Rsvd CPUs", "<b>The number of reserved CPU cores</b>"), "req_cpus", "req_cpus", "", false),
+            (("Rsvd GPUs", "<b>The number of reserved GPU cards</b>"), "req_gpus", "req_gpus", "", false),
+            (("Rsvd Mem", "<b>The amount of reserved RAM, in GB</b>"), "req_mem", "req_mem", "GB", false),
+            (("CPU Usage", "<b>CPU Usage Efficiency</b><br><br>The integral load of all CPUs in use divided by the number of reserved CPUs, in %.<br><br>If low (indicated by the reddish background), consider a <a href=\"https://www.niu.edu/crcd/current-users/getting-started/queue-commands-job-management.shtml#jobcontrol\">workflow optimization</a>."), "cpu_efficiency", "cpu_efficiency", "", true),
+            (("Memory Usage", "<b>Memory Usage Efficiency</b><br><br>The total amount of memory in use divided by the amount of reserved memory, in %.<br><br>If low (indicated by the reddish background), consider a <a href=\"https://www.niu.edu/crcd/current-users/getting-started/queue-commands-job-management.shtml#jobcontrol\">workflow optimization</a>."), "mem_efficiency", "mem_efficiency", "", true),
+            (("Elapsed Walltime", "<b>Total elapsed walltime/Reserved walltime, in %"), "walltime_efficiency", "walltime_efficiency", "", true)
         ].into_iter()
-            .map(|(name, sort_by, value, value_units, colored)| TableEntry {
+            .map(|((name, _tooltip), sort_by, value, value_units, colored)| TableEntry {
                 name: name.to_string(),
                 sort_by: sort_by.to_string(),
                 value: value.to_string(),
